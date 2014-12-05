cba644774bdf1ca27ffc4e76feae41edb242909d
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-12-05 11:54:05 -0500

    up to adding new articles

--- app/models/article.js
+++ app/models/article.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  createdAt: DS.attr('date'),
+  description: DS.attr('string'),
+  friend: DS.belongsTo('friend'),
+  state: DS.attr('string'),
+  notes: DS.attr('string')
 });
--- app/router.js
+++ app/router.js
@@ -8,12 +8,13 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource('friends', function() {
     this.route('new');
-    this.route('show', { path: ':friend_id' });
+    this.route('show', { path: ':friend_id' }, function() {
+      this.resource('articles', function() { });
+    });
     this.route('edit', { path: ':friend_id/edit'});
   });
-  this.route('friends/show');
-  this.route('friends/edit');
-  this.resource('articles', function() { });
+  // this.route('friends/show');
+  // this.route('friends/edit');
 });
 
 export default Router;
