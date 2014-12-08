8545fab539a588d1c117f3ac7cb4f682791bb0a2
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-12-07 22:37:01 -0500

    remove unsaved model from cancel

--- app/routes/friends/new.js
+++ app/routes/friends/new.js
@@ -4,6 +4,16 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('friend');
   },
+  deactivate: function() {
+    // grab the model we loaded in this route
+    var model = this.modelFor('friends/new');
+
+    // if we are leaving the route we check if the model
+    // is in 'isNew' state, which means it wasn't saved to the backend
+    if (model.get('isNew')) {
+      model.destroyRecord();
+    }
+  },
   actions: {
     save: function() {
       console.log('+-- save action bubbled up to friends new route');
