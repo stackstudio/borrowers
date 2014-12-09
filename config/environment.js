bad64a4f246ba8a507440f670f7fe24c2d752a90
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-12-09 18:18:34 -0500

    Ember.Select is deprecated
    http://emberjs.com/guides/deprecations/#toc_global-lookup-of-views-since
    -1-8

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,10 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    contentSecurityPolicy: {
+      'img-src': "'self' http://www.fillmurray.com",
+      'style-src': "'self' 'unsafe-inline'"
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
