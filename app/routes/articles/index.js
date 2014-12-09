ddc338c62c5b33895e3aed440e56d040a5b4ceae
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-12-09 17:39:47 -0500

    playing with ember-data

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -2,7 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.modelFor('friends/show').get('articles');
+    // return this.modelFor('friends/show').get('articles');
+    var articles = this.modelFor('friends/show').get('articles');
+
+    if (articles.get('isFulfilled')) {
+      console.log("----reloading articles----");
+      // articles.reload();
+    }
+
+    return articles;
   },
   actions: {
     save: function(model) {
