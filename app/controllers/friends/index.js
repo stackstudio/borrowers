2cad37da5822452bfeb14a4374d6ad1578bc1062
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-12-10 21:15:12 -0500

    change sort order dynamically

--- app/controllers/friends/index.js
+++ app/controllers/friends/index.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   sortAscending: true,
-  sortProperties: ['fullName']
+  sortBy: 'fullName',
+  sortProperties: Ember.computed('sortBy', function() {
+    return [this.get('sortBy')];
+  }),
+  actions: {
+    setSortBy: function(fieldName) {
+      this.set('sortBy', fieldName);
+      this.toggleProperty('sortAscending');
+
+      return false;
+    }
+  }
 });
