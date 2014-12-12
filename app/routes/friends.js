6074719011f73c7a82b0545ed9bc81381382db15
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-12-12 18:00:09 -0500

    confirm alert for friend delete from friends.index

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -11,8 +11,24 @@ export default Ember.Route.extend({
       return true;
     },
     delete: function(friend) {
-      friend.destroyRecord();
-      this.transitionTo('friends.index');
+      var _this = this,
+          _friend = friend;
+          
+      swal({
+        title: "Are you sure?",
+        text: "You will not be able to recover " + friend.get('firstName') + "!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Delete",
+        closeOnConfirm: false
+      },
+      function() {
+        friend.destroyRecord().then(function() {
+          swal("Deleted!", friend.get('fullName'), "success");
+          _this.transitionTo('friends.index');
+        });
+      });
     }
   }
 });
