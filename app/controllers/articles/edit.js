6fc355235a7d2253377186ad52ad73314fb07d5b
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-12-11 23:34:23 -0500

    implement articles/edit

--- app/controllers/articles/edit.js
+++ app/controllers/articles/edit.js
@@ -1,4 +1,5 @@
-import Ember from 'ember';
+import ArticlesBaseController from './base';
 
-export default Ember.Controller.extend({
+export default ArticlesBaseController.extend({
+
 });
--- app/routes/articles/edit.js
+++ app/routes/articles/edit.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+deactivate: function() {
+  var model = this.modelFor('articles.edit');
+
+  if (model.get('isDirty')) {
+    model.rollback();
+  }
+},
+actions: {
+  save: function() {
+    var _this = this;
+    var model = this.modelFor('articles.edit');
+    model.save().then(function() {
+      _this.transitionTo('articles');
+    });
+  },
+  cancel: function() {
+    this.transitionTo('articles');
+  }
+}
 });
