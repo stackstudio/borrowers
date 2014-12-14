0c399950c21d7660e24ded86e0b1881357f26cd2
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-12-14 01:14:12 -0500

    standardization

--- app/routes/articles/new.js
+++ app/routes/articles/new.js
@@ -16,7 +16,7 @@ export default Ember.Route.extend({
   actions: {
     save: function() {
       var _this = this;
-      var model = this.modelFor('articles.new');
+      var model = this.modelFor('articles/new');
       model.save().then(function() {
         _this.transitionTo('articles');
       });
