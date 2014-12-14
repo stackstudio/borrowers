1ea92c5b320000ca8accd468fc9044becbc70f78
Alan Hamilton <alan.b.hamilton@gmail.com>
2014-12-14 00:49:23 -0500

    add total articles lent to dashboard

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,17 @@ import request from 'ic-ajax';
 export default Ember.Route.extend({
   model: function() {
     return request('api/friends').then(function(data) {
-      return { friendsCount: data.friends.length };
+      return {
+        friendsCount: data.friends.length,
+        articlesCount: function() {
+          var count = 0;
+
+          data.friends.forEach(function(friend) {
+            count += friend.total_articles;
+          });
+          return count;
+        }()
+      };
     });
   }
 });
